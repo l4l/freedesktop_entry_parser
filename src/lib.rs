acc825340aa20ac46b39a8c29be48e516c9e3e35
zethra <jediben97@gmail.com>
2020-06-13 18:21:00 -0400

    Improved docs a bit
    Signed-off-by: zethra <jediben97@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,15 @@
 
 //! A library for parsing FreeDesktop entry files.
 //! These files are used in the
-//! [Desktop Entry](https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html)
-//! and [Icon Theme](https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html).
+//! [Desktop Entry files](https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html),
+//! [Icon Theme index files](https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html),
+//! and [Systemd unit files](https://www.freedesktop.org/software/systemd/man/systemd.unit.html).
 //! They are similar to ini files but are distinct enough that
 //! an ini parse would not work.
 //!
 //! Example:
 //! ```
-//! use freedesktop_entry_parser::*;
+//! use freedesktop_entry_parser::{parse_entry, SectionBytes, AttrBytes};
 //!
 //! let file = b"[Desktop Entry]
 //! Name=Firefox
@@ -26,7 +27,7 @@
 //!         AttrBytes { name: b"Icon", value: b"firefox"},
 //!     ]
 //! });
-//! # Ok::<(), ParseError>(())
+//! # Ok::<(), freedesktop_entry_parser::ParseError>(())
 //! ```
 
 mod debug;
